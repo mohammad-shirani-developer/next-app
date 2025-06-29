c8d72ad856583c14db10619124ba33e34defe787
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-06-29 10:57:21 +0330

    Client and server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCart from "./components/ProductCart";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
       <Link href="/users">Users</Link>
+      <ProductCart />
     </main>
   );
 }
