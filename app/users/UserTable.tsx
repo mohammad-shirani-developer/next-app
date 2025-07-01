3ff93ea472991b15a097ad489fb2bd4369cb5c5e
Mohammad-Shrani <mohammad.shirani.developer@gmail.com>
2025-07-01 09:07:18 +0330

    Create error page

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 const UserTable = async ({ sortOrder }: Props) => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/xusers");
   const users: User[] = await res.json();
   const sortedUsers = sort(users).asc(
     sortOrder === "email" ? (user) => user.email : (user) => user.name
