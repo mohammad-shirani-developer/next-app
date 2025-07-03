832dcb854a7c438b954436e87881e70554354bdc
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-07-03 16:15:20 +0330

    Creating user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 import { prisma } from "@/prisma/client";
+import { email } from "zod/v4";
 
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany();
@@ -13,5 +14,19 @@ export async function POST(request: NextRequest) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
